1a442f06b74add4a6464b2a58e466d93920bf0e5
qu-shao <992725803@qq.com>
2023-03-08 10:55:52 +0800

    modified:   package-lock.json 	modified:   package.json 	modified:   src/main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App).mount('#app')
+
+app.use(ElementPlus, {size: "small", zIndex: 3000 })
+app.mount('#app')
